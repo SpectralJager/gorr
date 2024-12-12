0ccfede7be15888ab247aa5507e8f001bf796697
spectrum <unkn.spectrum@gmail.com>
2024-12-13 00:06:40 +0700

    torrent file parsing complite

--- gorr_test.go
+++ gorr_test.go
@@ -1,1 +1,22 @@
 package gorr
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/SpectralJager/gorr/bencode"
+)
+
+func TestParseTorrent(t *testing.T) {
+	input, _ := os.ReadFile("test/torrent.torrent")
+	document, err := bencode.NewDecoder(bytes.NewReader(input), len(input)).Decode()
+	if err != nil {
+		t.Fatal(err)
+	}
+	torrentFile := NewTorrent(document)
+	data, _ := json.MarshalIndent(torrentFile, "", "  ")
+	fmt.Println(string(data))
+}
